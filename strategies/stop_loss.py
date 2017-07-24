503b616bb519cb01b7723942b135089f21884a7e
Nathan Merritt <gnm@the-merritts.net>
2017-07-23 23:10:12 -0400

    Simple implementation of stop-loss

---
+++ strategies/stop_loss.py
@@ -0,0 +1,19 @@
+from db import Ticker
+
+DROP_PERCENT = 0.1
+
+
+def run_strategy(sess, now, ticker, account, debug=False):
+    if account.balance(ticker) == 0:
+        return None
+    open_time = account.opened(ticker)
+    peak = Ticker.peak(sess, ticker, start_time=open_time, now=now)
+    current = Ticker.current_ask(sess, ticker, now)
+    if current is None or peak is None:
+        return None
+
+    drop_percent = (current - peak) / peak
+    if drop_percent < -DROP_PERCENT:
+        print("Sell of '{}' ask {} @ {} (down {}%)"
+              .format(ticker, current, now, drop_percent * 100))
+        return -1, current
