7e210fc870b3ad4d73bc078c2ac8df5d73717300
Nathan Merritt <gnm@the-merritts.net>
2017-07-23 20:29:47 -0400

    Helpful queries on Ticker

--- strategies/db.py
+++ strategies/db.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Float, String, DateTime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
 
 
 Base = declarative_base()
@@ -34,6 +35,32 @@ class Ticker(Base):
     def __repr__(self):
         return "Ticker(exchange={}, coin={}, last={}, timestamp={})" \
                .format(self.exchange, self.coin, self.last, self.timestamp)
+
+    @staticmethod
+    def peak(sess, coin, start_time=None, now=None):
+        query = Ticker.at_time(
+            sess.query(func.max(Ticker.last)).filter(Ticker.coin == coin),
+            now)
+        if start_time:
+            query = query.filter(Ticker.timestamp > start_time)
+        res = query.first()
+        return res[0] if res else None
+
+    @staticmethod
+    def current_ask(sess, coin, now=None):
+        query = Ticker.at_time(
+            sess.query(Ticker.ask)
+                .filter(Ticker.coin == coin)
+                .order_by(Ticker.timestamp.desc()),
+            now)
+        ask = query.first()
+        return ask[0] if ask else None
+
+    @staticmethod
+    def at_time(query, now=None):
+        if now is None:
+            return query
+        return query.filter(Ticker.timestamp <= now)
 
 
 def create_db(db_string):
