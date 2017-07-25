b06e7686fb3275ccbb8c49f691cb4a7dc6bb93a3
Nathan Merritt <gnm@the-merritts.net>
2017-07-24 21:40:21 -0400

    Add comparison to buy-and-hold

--- strategies/backtest.py
+++ strategies/backtest.py
@@ -13,13 +13,13 @@ def fetch_data_timestamp(sess, oldest=True):
         .timestamp
 
 
-def account_value_btc(sess, account):
+def account_value_btc(sess, account, now=None):
     btc = account.balance('BTC')
     for coin in account.coins:
         if coin == 'BTC':
             continue  # BTC is priced in USD, everything else in BTC
         units = account.balance(coin)
-        unit_price = Ticker.current_ask(sess, coin)
+        unit_price = Ticker.current_ask(sess, coin, now)
         btc += units * unit_price
     return btc
 
@@ -35,6 +35,10 @@ class Backtester(object):
         self.step = step
 
     def run_backtest(self):
+        self.run_strategy()
+        self.buy_and_hold()
+
+    def run_strategy(self):
         print("Backtesting for currencies: {}".format(self.coins))
         print("Running backtest between {}->{} at {} intervals"
               .format(self.start_data, self.now, self.step))
@@ -57,7 +61,35 @@ class Backtester(object):
         print("\nBalance after running backest ({}): {} BTC\n"
               .format(self.now, finish_value))
         print("Paid {} BTC in fees".format(account.fees))
-        print("Return over period: {}".format(percent_return))
+        print("Return over period: {}%".format(round(percent_return)))
+
+        return percent_return
+
+    def buy_and_hold(self):
+        account = Account(self.balances)
+        start = self.start_data + timedelta(hours=2)
+        # one share of each alt, one share of BTC
+        btc_per_coin = account.balance('BTC') / (len(self.coins) + 1)
+
+        for i, coin in enumerate(self.coins):
+            with_fees = btc_per_coin - (btc_per_coin * 0.0025)
+            price = Ticker.current_ask(self.sess, coin, now=start)
+            to_buy = with_fees / price
+            cost = account.trade(coin, to_buy, price, start)
+            account.update('BTC', cost, self.start_data)
+
+        start_value = account_value_btc(self.sess, account, now=start)
+        print("\nBuy and hold\n")
+        print("Account with initial buys: {}".format(account))
+        print("Initial value: {} BTC".format(start_value))
+        finish_value = account_value_btc(self.sess, account, self.now)
+        percent_return = 100 * (finish_value - start_value) / finish_value
+        print("Balance after buy and hold ({}): {} BTC"
+              .format(self.now, finish_value))
+        print("Paid {} BTC in fees".format(account.fees))
+        print("Return over period: {}%".format(round(percent_return, 2)))
+
+        return percent_return
 
     def backtest_period(self, period, account):
         for coin in self.coins:
