ed3e7fa08beee7541979349c31076e791b087a7b
Nathan Merritt <gnm@the-merritts.net>
2017-07-31 19:43:44 +0000

    Hold coins for a minimum amount of time

--- strategies/stop_loss.py
+++ strategies/stop_loss.py
@@ -1,17 +1,24 @@
 import logging
+from datetime import timedelta
 
 from db import Ticker
 
 log = logging.getLogger('default')
 
 DROP_PERCENT = 0.04
+MIN_HOLD_TIME = timedelta(hours=24)
 
 
 def run_strategy(sess, now, ticker, account, debug=False):
     if account.balance(ticker) == 0:
         return None
     open_time = account.opened(ticker)
-    peak = Ticker.peak(sess, ticker, start_time=open_time, now=now)
+    first_sell = open_time + MIN_HOLD_TIME
+    if now < first_sell:
+        log.debug("Min holding time hasn't passed yet for {}".format(ticker))
+        return None
+
+    peak = Ticker.peak(sess, ticker, start_time=first_sell, now=now)
     current = Ticker.current_ask(sess, ticker, now)
     if current is None or peak is None:
         return None
