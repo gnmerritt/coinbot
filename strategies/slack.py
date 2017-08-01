645936143fa9b9216e02c2b2c2cf56cf8cd005ab
Nathan Merritt <gnm@the-merritts.net>
2017-08-01 20:28:20 +0000

    different bot icons

--- strategies/slack.py
+++ strategies/slack.py
@@ -1,14 +1,15 @@
 import logging
 from slacker_log_handler import SlackerLogHandler, NoStacktraceFormatter
 
-FMT = '*[%(levelname)s]* <%(name)s> %(message)s'
+FMT = '*[%(levelname)s]* %(message)s'
 
 
 def setup_loggers(slack):
     for name in ['default', 'txns', 'cron']:
+        username = "{}-{}".format(slack['username'], name)
         handler = SlackerLogHandler(
             slack['key'], slack['channel'],
-            username=slack['username'], icon_emoji=slack['emoji']
+            username=username, icon_emoji=slack['emojis'].get(name)
         )
 
         formatter = NoStacktraceFormatter(FMT)
