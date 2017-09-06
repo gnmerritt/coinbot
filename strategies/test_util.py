16a11d56e1fa6cbcd034179a9dd64cdfb1b804d0
Nathan Merritt <gnm@the-merritts.net>
2017-09-06 08:14:15 -0400

    Another stab at fixing fp rounding

--- strategies/test_util.py
+++ strategies/test_util.py
@@ -8,5 +8,9 @@ class TestUtil(unittest.TestCase):
     def test_rounding(self):
         self.assertEqual(Decimal('11188.36140127'),
                          crypto_truncate(11188.361401279999))
+        self.assertEqual(Decimal('11188.36140127'),
+                         crypto_truncate(Decimal(11188.361401279999)))
         self.assertEqual(11188.36140127,
                          float(crypto_truncate(11188.361401279999)))
+        self.assertEqual(Decimal('-2162.60633012'),
+                         crypto_truncate(-2162.60633012))
--- strategies/util.py
+++ strategies/util.py
@@ -1,8 +1,6 @@
-from decimal import localcontext, ROUND_DOWN, Decimal
+from decimal import ROUND_DOWN, Decimal
 
 
 def crypto_truncate(amount):
     d = Decimal(amount)
-    with localcontext() as ctx:
-        ctx.rounding = ROUND_DOWN
-        return d.quantize(Decimal('0.00000001'))
+    return d.quantize(Decimal('0.00000001'), rounding=ROUND_DOWN)
